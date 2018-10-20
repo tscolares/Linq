ea700be90ddb42aefa5970a9b7df259cc5250066
Thiago Colares <tscolares@live.com>
2018-10-20 15:05:11 -0300

    Projeto Console App C# exemplo de Linq com Lambda Expressions

---
+++ LinqWithLambdaSolution/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqWithLambdaSolution.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
